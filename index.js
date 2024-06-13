faa965127673bc88f592dc7808d32eb5eadad729
Alexandr Rudskiy <BowieAlex23@gmail.com>
2024-06-13 11:10:27 +0300

    actions

--- index.js
+++ index.js
@@ -4,32 +4,32 @@ import _ from 'lodash';
 import { fileURLToPath } from 'url';
 
 const readFile = (filepath) => {
-    const pathF = path.resolve(process.cwd(), filepath);
-    return JSON.parse(fs.readFileSync(pathF, 'utf-8'));
+  const pathF = path.resolve(process.cwd(), filepath);
+  return JSON.parse(fs.readFileSync(pathF, 'utf-8'));
 };
 
 const gendiff = (filepath1, filepath2) => {
-    const data1 = readFile(filepath1);
-    const data2 = readFile(filepath2);
-    
-    const keys = _.union(_.keys(data1), _.keys(data2)).sort();
-    const diffObj = keys.map((key) => {
-        if (_.has(data1, key) && !_.has(data2, key)) {
-            return `- ${key}: ${data1[key]}`;
-        }
+  const data1 = readFile(filepath1);
+  const data2 = readFile(filepath2);
 
-        if (_.has(data2, key) && !_.has(data1, key)) {
-            return `  + ${key}: ${data2[key]}`
-        }
+  const keys = _.union(_.keys(data1), _.keys(data2)).sort();
+  const diffObj = keys.map((key) => {
+    if (_.has(data1, key) && !_.has(data2, key)) {
+      return `- ${key}: ${data1[key]}`;
+    }
 
-        if (data1[key] !== data2[key]) {
-            return `  - ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`
-        }
+    if (_.has(data2, key) && !_.has(data1, key)) {
+      return `  + ${key}: ${data2[key]}`;
+    }
 
-        return `   ${key}: ${data1[key]}`;
-      })
+    if (data1[key] !== data2[key]) {
+      return `  - ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
+    }
 
-    return `{\n${diffObj.join('\n')}\n}`;
-    };
+    return `   ${key}: ${data1[key]}`;
+  });
+
+  return `{\n${diffObj.join('\n')}\n}`;
+};
 
 export default gendiff;
